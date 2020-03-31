da4e0f2ded42f890112df710cd4034711090b46b
Vipul Swarup <vipulswarup@Vipuls-MacBook-Air.local>
2020-03-31 16:26:20 +0530

    more changes

--- calculator.js
+++ calculator.js
@@ -1,4 +1,18 @@
-function init() {
+//Global calculation
+var globalCalculation = 0;
+var lastClicked = 0;
 
+function changeDisplay(numberToShow) {
+  //alert(numberToShow);
+  var fieldNameElement = document.getElementById("calcDisplay");
+  fieldNameElement.innerHTML = numberToShow;
+}
+function init() {
+  document
+    .querySelector(".calculatorBody")
+    .addEventListener("click", function(event) {
+      lastClicked = `${event.target.innerText}`;
+      changeDisplay(lastClicked);
+    });
 }
 init();
